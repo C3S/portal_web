f092349685152baf22592436cf1fa9aa0d5ca3e0
Alexander Blum <alexander.blum@c3s.cc>
2019-05-25 21:44:38 +0200

    enhances benchmark debugging

--- collecting_society_portal/services/benchmark.py
+++ collecting_society_portal/services/benchmark.py
@@ -3,11 +3,13 @@
 
 import os
 import time
+import datetime
+import logging
+
 from . import (
     csv_import,
     csv_export
 )
-
 
 benchmark_file = "/ado/tmp/benchmark/benchmark.csv"
 csv_config = {
@@ -25,15 +27,22 @@ csv_fieldnames = [
     'result'
 ]
 
+log = logging.getLogger(__name__)
 
-class benchmark(object):
-    def __init__(self, request, name, uid, normalize=1.0, scale=1.0,
-                 environment='development'):
+
+class benchmark():
+    def __init__(self, request, name,
+                 uid=datetime.datetime.now().strftime('%a, %d %b %Y %H:%M:%S'),
+                 normalize=1.0, scale=1.0, environment='development'):
         self.request = request
-        if self.request.registry.settings.get('benchmark.' + name) != 'true':
+        self.skip = False
+        if self.request.registry.settings['env'] != environment:
             self.skip = True
+        if self.request.registry.settings.get(
+                'benchmark.' + name) != 'true':
+            self.skip = True
+        if self.skip:
             return
-        self.skip = (self.request.registry.settings['env'] != environment)
         self.name = name
         self.uid = uid
         self.scale = scale
@@ -86,7 +95,9 @@ class benchmark(object):
         return False
 
 
-def benchmarks():
+def benchmarks(delete=False):
+    if delete and os.path.isfile(benchmark_file):
+        os.remove(benchmark_file)
     if not os.path.isfile(benchmark_file):
         return {'benchmarks': None, 'results': None}
 
