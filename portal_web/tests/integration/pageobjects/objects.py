c3a36b5f06595fd4a44f0a9a2bc6361be3cde10a
Alexander Blum <alexander.blum@c3s.cc>
2023-04-11 17:35:23 +0200

    adds ability to instanciate DeformFormObject forms with request/userid

--- portal_web/tests/integration/pageobjects/objects.py
+++ portal_web/tests/integration/pageobjects/objects.py
@@ -1,11 +1,23 @@
 # For copyright and license terms, see COPYRIGHT.rst (top level of repository)
 # Repository: https://github.com/C3S/portal_web
 
+from inspect import signature
 import deform
 import sys
+import os
 
+from pyramid import testing
+from paste.deploy.loadwsgi import appconfig
+
+from ....config import (
+    get_plugins,
+    replace_environment_vars,
+    web_user,
+    party,
+    user,
+    roles,
+)
 from ....views.forms.datatables import DatatableSequenceWidget
-
 from .elements import (  # noqa: F401
     TextInputWidgetElement,
     TextAreaWidgetElement,
@@ -24,11 +36,41 @@ from .elements import (  # noqa: F401
 __all__ = ['DeformFormObject']
 
 
+def settings():
+    """
+    Provides parsed pyramid settings (plugins combined, envvars substituted).
+    """
+    environment = 'testing'
+    settings = appconfig(
+        'config:' + os.path.join(
+            os.path.dirname(__file__), '..', '..', '..', '..',
+            f'{environment}.ini'
+        )
+    )
+    plugins = get_plugins(settings, environment)
+    for priority in sorted(plugins, reverse=True):
+        settings.update(plugins[priority]['settings'])
+    settings = replace_environment_vars(settings)
+    return settings
+
+
 class DeformFormObject(object):
 
-    def __init__(self, browser, form, formid):
-        if not isinstance(form, deform.Form):
-            raise TypeError('form is not an instance of deform.Form')
+    def __init__(self, browser, form, formid, userid=None):
+        if callable(form):
+            sig = signature(form)
+            if sig.parameters.get('request'):
+                request = testing.DummyRequest()
+                config = testing.setUp(request=request, settings=settings())
+                if userid:
+                    config.testing_securitypolicy(userid)
+                    request.web_user = web_user(request)
+                    request.party = party(request)
+                    request.user = user(request)
+                    request.roles = roles(request)
+                form = form(request)
+            else:
+                form = form()
         self._browser = browser
         self._form = form
         self._formid = formid
